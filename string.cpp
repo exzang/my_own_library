851a1d2f1c49b647d44235fd94f03e1bb341f0f0
exzang <exzang893@gmail.com>
2026-04-01 14:50:57 +0800

    Add suffix array implementation in string.cpp

---
+++ string.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+constexpr int N = 1000010;
+
+char s[N];
+int n;
+int m, p, rk[N * 2], oldrk[N], sa[N * 2], id[N], cnt[N];
+
+int main() {
+  scanf("%s", s + 1);
+  n = strlen(s + 1);
+  m = 128;
+
+  for (int i = 1; i <= n; i++) cnt[rk[i] = s[i]]++;
+  for (int i = 1; i <= m; i++) cnt[i] += cnt[i - 1];
+  for (int i = n; i >= 1; i--) sa[cnt[rk[i]]--] = i;
+
+  for (int w = 1;; w <<= 1, m = p) {  // m = p 即为值域优化
+    int cur = 0;
+    for (int i = n - w + 1; i <= n; i++) id[++cur] = i;
+    for (int i = 1; i <= n; i++)
+      if (sa[i] > w) id[++cur] = sa[i] - w;
+
+    memset(cnt, 0, sizeof(cnt));
+    for (int i = 1; i <= n; i++) cnt[rk[i]]++;
+    for (int i = 1; i <= m; i++) cnt[i] += cnt[i - 1];
+    for (int i = n; i >= 1; i--) sa[cnt[rk[id[i]]]--] = id[i];
+
+    p = 0;
+    memcpy(oldrk, rk, sizeof(oldrk));
+    for (int i = 1; i <= n; i++) {
+      if (oldrk[sa[i]] == oldrk[sa[i - 1]] &&
+          oldrk[sa[i] + w] == oldrk[sa[i - 1] + w])
+        rk[sa[i]] = p;
+      else
+        rk[sa[i]] = ++p;
+    }
+
+    if (p == n) break;  // p = n 时无需再排序
+  }
+
+
+  for (i = 1, k = 0; i <= n; ++i) {
+  if (rk[i] == 0) continue;
+  if (k) --k;
+  while (s[i + k] == s[sa[rk[i] - 1] + k]) ++k;
+  height[rk[i]] = k;
+}
+  return 0;
+}
