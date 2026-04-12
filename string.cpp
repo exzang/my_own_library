30a854a99d78228161d06f1c748feb79e59fd831
exzang <exzang893@gmail.com>
2026-04-13 05:19:04 +0800

    Update string.cpp

--- string.cpp
+++ string.cpp
@@ -119,3 +119,58 @@ struct treap {
 		prep(ls[x]),prep(rs[x]);
 	}
 } trp;
+
+
+struct SuffixArray {
+  char S[N]; int n; 
+  int cnt[N], sa[N], rk[N], height[N]; 
+  int st[N][25], lg2[N];
+  struct node {
+    int id, x, y; 
+  }aa[N], bb[N];
+  inline void buildsa() {
+    n = strlen(S + 1);
+    memset(cnt, 0, sizeof(cnt)); 
+    memset(height, 0, sizeof(height));
+    memset(sa, 0, sizeof(sa));
+    memset(rk, 0, sizeof(rk)); 
+    for(int i = 1; i <= n; i++) aa[i].id = bb[i].id = aa[i].x = aa[i].y = bb[i].x = bb[i].y = 0; 
+    for(int i = 1; i <= n; i++) cnt[S[i]] = 1; 
+    for(int i = 1; i <= 256; i++) cnt[i] += cnt[i - 1];
+    for(int i = 1; i <= n; i++) rk[i] = cnt[S[i]];
+    for(int L = 1; L < n; L *= 2) {
+      for(int i = 1; i <= n; i++) aa[i].id = i, aa[i].x = rk[i], aa[i].y = rk[i + L]; 
+      for(int i = 1; i <= n; i++) cnt[i] = 0;
+      for(int i = 1; i <= n; i++) cnt[aa[i].y]++;
+      for(int i = 1; i <= n; i++) cnt[i] += cnt[i - 1];
+      for(int i = n; i >= 1; i--) bb[cnt[aa[i].y]--] = aa[i];
+      for(int i = 1; i <= n; i++) cnt[i] = 0;
+      for(int i = 1; i <= n; i++) cnt[aa[i].x]++;
+      for(int i = 1; i <= n; i++) cnt[i] += cnt[i - 1];
+      for(int i = n; i >= 1; i--) aa[cnt[bb[i].x]--] = bb[i];
+      for(int i = 1; i <= n; i++) 
+        if(aa[i].x == aa[i - 1].x && aa[i].y == aa[i - 1].y)
+          rk[aa[i].id] = rk[aa[i - 1].id];
+        else rk[aa[i].id] = rk[aa[i - 1].id] + 1; 
+    } for(int i = 1; i <= n; i++) sa[rk[i]] = i; int k = 0; 
+    for(int i = 1; i <= n; i++) {
+      if(k) k--;
+      int j = sa[rk[i] - 1];
+      while(i + k <= n && j + k <= n && S[i + k] == S[j + k]) k++;
+      height[rk[i]] = k;
+    }
+  }
+  inline void buildst() {
+    lg2[0] = -1; for(int i = 1; i < N; i++) lg2[i] = lg2[i / 2] + 1; lg2[0] = 0; 
+    for(int i = 1; i <= n; i++) st[i][0] = height[i]; 
+    for(int j = 1; (1 << j) <= n; j++)
+      for(int i = 1; i + (1 << j) - 1 <= n; i++)
+        st[i][j] = min(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
+  }
+  inline int Lcp(int l, int r) {
+    l = rk[l], r = rk[r];
+    if(l > r) swap(l, r); l++; 
+    int k = lg2[r - l + 1]; 
+    return min(st[l][k], st[r - (1 << k) + 1][k]); 
+  }
+}SA[2]; 
